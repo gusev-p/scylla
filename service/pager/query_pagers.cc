da5e3e10d5b1aa80ce89f4a6173cfe77e3ad3680
Petr Gusev <petr.gusev@scylladb.com>
2025-05-22 13:14:39 +0200

    query_options: add replica_local mode
    We want to access the paxos state table only on the local node and
    shard (or shards in case of intranode_migration). In this commit we
    add a replica_local flag to query_options, which allows to do that.
    This flag can be set for a query via make_internal_options.
    
    We handle this flag on the statements layer, in select_statement
    and modification_statement. This

--- service/pager/query_pagers.cc
+++ service/pager/query_pagers.cc
@@ -193,7 +193,7 @@ future<result<service::storage_proxy::coordinator_query_result>> query_pager::do
             std::move(command),
             std::move(ranges),
             _options.get_consistency(),
-            {timeout, _state.get_permit(), _state.get_client_state(), _state.get_trace_state(), std::move(_last_replicas), _query_read_repair_decision});
+            {timeout, _state.get_permit(), _state.get_client_state(), _state.get_trace_state(), std::move(_last_replicas), _query_read_repair_decision, _options.get_specific_options().replica_local});
 }
 
 future<> query_pager::fetch_page(cql3::selection::result_set_builder& builder, uint32_t page_size, gc_clock::time_point now, db::timeout_clock::time_point timeout) {
